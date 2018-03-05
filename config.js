96b72229c8910046367e8dcae3ed828216edae7e
wojjar <jaroslaw@wojcik.hub.pl>
2018-03-05 21:04:00 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+
+module.exports = {
+    'GOOGLE_CLIENT_ID': '748919886357-93fjlvk6kffnofqcp0fqiueg4h4kqjfr.apps.googleusercontent.com',
+    'GOOGLE_CLIENT_SECRET': '4rfayJ1Abw4wjKkwPXIiNXPv',
+    'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
+
+
+
